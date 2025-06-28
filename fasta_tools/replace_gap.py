93c0b6c17692ecac76ef3bccbc9be58a83bd6fd7
Rajiv Boscolo Agostini <134699560+raj94b@users.noreply.github.com>
2025-06-28 19:59:32 +0200

    Update and rename replace_gap_in_fasta.py to fasta_tools/replace_gap.py

--- fasta_tools/replace_gap.py
+++ fasta_tools/replace_gap.py
@@ -5,8 +5,8 @@ Created on Thu Nov  4 11:23:31 2021
 @author: Rajiv
 """
 #replace gaps - with n
-fasta_file_read = open("C:/Users/Rajiv/Documents/mito/mito_2_1r.fasta", "r") #read fasta
-fasta_file_new = open("C:/Users/Rajiv/Documents/mito/mito_new_seq_aligned_final.fasta", "w") #write new fasta
+fasta_file_read = open("mito_2_1r.fasta", "r") #read fasta
+fasta_file_new = open("mito_new_seq_aligned_final.fasta", "w") #write new fasta
 
 for f in fasta_file_read.readlines():
     if ">" in f:
